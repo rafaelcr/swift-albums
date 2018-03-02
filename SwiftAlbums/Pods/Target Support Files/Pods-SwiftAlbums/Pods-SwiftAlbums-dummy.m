471484c7a387d324a4ccb119c77dac39bfa030b5
Rafael Cardenas <rafael@rafaelcr.com>
2018-03-01 22:57:18 -0600

    Initial commit. Pods added

---
+++ SwiftAlbums/Pods/Target Support Files/Pods-SwiftAlbums/Pods-SwiftAlbums-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftAlbums : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftAlbums
+@end
---
+++ SwiftAlbums/SwiftAlbums/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAlbums
+//
+//  Created by Rafael Cardenas on 3/1/18.
+//  Copyright © 2018 Rafael Cárdenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
