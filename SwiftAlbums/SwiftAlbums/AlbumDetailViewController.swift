3902ca37d8f422f0c0bef0b483592ef638920f0e
Rafael Cardenas <rafael@rafaelcr.com>
2018-03-06 20:25:15 -0600

    Working album detail

--- SwiftAlbums/SwiftAlbums/AlbumDetailViewController.swift
+++ SwiftAlbums/SwiftAlbums/AlbumDetailViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Rafael Cardenas on 3/6/18.
 //  Copyright © 2018 Rafael Cárdenas. All rights reserved.
 //
-
+import AlamofireImage
 import Foundation
 import UIKit
 
@@ -16,5 +16,12 @@ class AlbumDetailViewController: UIViewController {
   @IBOutlet var cover: UIImageView!
   @IBOutlet var titleLabel: UILabel!
   @IBOutlet var descriptionLabel: UILabel!
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    cover.af_setImage(withURL: URL(string: album.coverURL)!)
+    titleLabel.text = album.name
+    descriptionLabel.text = album.description
+  }
 
 }
